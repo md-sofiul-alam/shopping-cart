7cef3db602feaa8a048ee03cf672e3eef26e6561
md-sofiul-alam <sowrobeakter@gmail.com>
2023-02-15 22:51:39 +0600

    cart remove and inputField isue fixed

--- script.js
+++ script.js
@@ -24,10 +24,22 @@ function totalCost(id1, id2, id3, id4) {
     const totalPrice = document.getElementById(id4);
     subTotalPrice.innerText = phonePrice + casePrice;
     totalPrice.innerText = phonePrice + casePrice;
-
-
-
 }
+
+function priceCartRemove(removeCartId, existingCartId, id3, id4){
+    removeCartId=0;
+    const casePriceString = document.getElementById(existingCartId);
+    const casePrice = parseInt(casePriceString.innerText);
+    const subTotalPrice = document.getElementById(id3);
+    const totalPrice = document.getElementById(id4);
+    subTotalPrice.innerText = removeCartId + casePrice;
+    totalPrice.innerText = removeCartId + casePrice;
+}
+function removeCart(id){
+    const element = document.getElementById(id);
+    element.remove();
+}
+
 document.getElementById('btn-phone-decrease').addEventListener('click', function () {
     increaseDecrease(false, 'phone-qty-field');
     price(1219, 'phone-qty-field', 'phone-price-total');
@@ -39,6 +51,12 @@ document.getElementById('btn-phone-increase').addEventListener('click', function
     price(1219, 'phone-qty-field', 'phone-price-total');
     totalCost('phone-price-total', 'case-price-total', 'sub-total', 'total');
 })
+
+document.getElementById('phone-remove').addEventListener('click', function(){
+    priceCartRemove('phone-price-total','case-price-total','sub-total', 'total' )
+    removeCart('phone-cart');
+})
+
 document.getElementById('btn-case-decrease').addEventListener('click', function () {
     increaseDecrease(false, 'case-count');
     price(59, 'case-count', 'case-price-total');
@@ -49,4 +67,8 @@ document.getElementById('btn-case-increase').addEventListener('click', function 
     increaseDecrease(true, 'case-count');
     price(59, 'case-count', 'case-price-total');
     totalCost('phone-price-total', 'case-price-total', 'sub-total', 'total');
+})
+document.getElementById('case-remove').addEventListener('click', function(){
+    priceCartRemove('case-price-total','phone-price-total','sub-total', 'total' )
+    removeCart('case-cart');
 })
